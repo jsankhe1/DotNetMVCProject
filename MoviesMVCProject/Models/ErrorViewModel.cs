2cd804f91f43d8647e0fd3b4363dacb5464c6f63
jsankhe1 <jsankhe1@binghamton.edu>
2025-01-30 17:46:54 -0500

    Create Initial Project Template

---
+++ MoviesMVCProject/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MoviesMVCProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
